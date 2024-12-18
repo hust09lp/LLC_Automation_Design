7d2c067c38c998049abe33cf38ae805b92644e89
hust09lp <hust09lp@gmail.com>
2024-12-18 20:13:11 +0800

    creat a class of the LLC converter

--- src/LLC_base.py
+++ src/LLC_base.py
@@ -1,3 +1,5 @@
+import math
+
 # define the LLC converter class
 class LLC:
     # 我的想法时最好直接传递一个输入参数字典{Vin, Vo, P, k, fr}过来
@@ -12,9 +14,6 @@ class LLC:
         self.k       = input_para[7]
         self.fr      = input_para[8]
 
-    # 计算谐振参数{Lr, Cr, Lm}
-    def calc(self):
-        # 首先计算隔离变压器匝比
         if self.Vin_n >= self.Vo_n:
             self.np2s = round(self.Vin_n / self.Vo_n)
             self.ns2p = 1 / self.np2s
@@ -23,12 +22,47 @@ class LLC:
             self.np2s = 1 / self.ns2p
 
         # 计算电路的最小最大增益
-        self.Gmin =
-        self.Gmax =
+        self.Gmin = self.np2s * self.Vo_min / self.Vin_max
+        self.Gmax = self.np2s * self.Vo_max / self.Vin_min
+
+        # 计算等效交流内阻
+        self.Rac_n = 2 * self.np2s**2 * self.Vo_n**2 / (math.pi**2 * self.Po)
+        self.Rac_min = 2 * self.np2s ** 2 * self.Vo_min ** 2 / (math.pi ** 2 * self.Po)
+        self.Rac_max = 2 * self.np2s ** 2 * self.Vo_max ** 2 / (math.pi ** 2 * self.Po)
 
         # 根据增益求解开关频率范围
-        self.fmax =
-        self.fmin =
+        self.fmin = self.fr / math.sqrt(self.k * (1 - 1/self.Gmax**2) + 1)
+        self.fmax = self.fr / math.sqrt(1 + self.k*(1-1/self.Gmin))
 
-    # 绘制增益图
-    def plot_G(self):
+        # 计算最大品质因数，保留0.95的裕量
+        self.Qmax = 0.95 * math.sqrt((self.Gmax**2 * (1+self.k) - self.k) / (self.Gmax**2 * self.k**2 * (self.Gmax**2-1)))
+
+        # 计算谐振参数
+        self.Lr = self.Qmax * self.Rac_min / (2 * math.pi * self.fr)
+        self.Cr = 1 / (self.Qmax * self.Rac_min * 2 * math.pi * self.fr)
+        self.Lm = self.k * self.Lr
+
+    def output(self):
+        print(f'Gmax is {self.Gmax}.')
+        print(f'Gmin is {self.Gmin}.')
+        print(f'Qmax is {self.Qmax}.')
+        print(f'Lr is {self.Lr} H.')
+        print(f'Cr is {self.Cr} F.')
+        print(f'Lm is {self.Lm} H.')
+
+# 做一个参数输入的交互
+Vin_n = float(input('Please input the nominal input voltage (V): '))
+Vin_min = float(input('Please input the minimum input voltage (V): '))
+Vin_max = float(input('Please input the maximum input voltage (V): '))
+Vo_n = float(input('Please input the nominal output voltage (V): '))
+Vo_min = float(input('Please input the minimum output voltage (V): '))
+Vo_max = float(input('Please input the maximum output voltage (V): '))
+P = float(input('Please input the nominal power (W): '))
+k = float(input('Please input the ratio of Lm/Lr: '))
+fr = float(input('please input the resonant frequency (Hz): '))
+
+para = [Vin_n, Vin_min, Vin_max, Vo_n, Vo_min, Vo_max, P, k, fr]
+
+# 新建对象，并输出参数
+LLC_converter = LLC(para)
+LLC_converter.output()
